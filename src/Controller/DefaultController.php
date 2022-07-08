f9d39afe06c130c9799c11a9588dd2c380c44be1
Heiner Lohaus <heiner.lohaus@netformic.com>
2022-07-08 21:58:41 +0200

    Initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    public function index(Request $request): JsonResponse
+    {
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+}
